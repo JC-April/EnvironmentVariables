446643461ec27272165534b84cc99ff4290143a2
JC-April <jimkronicles@gmail.com>
2022-09-19 00:38:18 +0100

    Environmental Variables Commit

---
+++ config.js
@@ -0,0 +1,4 @@
+export const config = {
+  firstname: process.env.FIRST_NAME,
+  lastname: process.env.LAST_NAME,
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+require("dotenv").config();
+
+console.log(`${process.env.FIRST_NAME}, ${process.env.LAST_NAME}`);
